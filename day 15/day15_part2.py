ff571412c968fe7eddb1222e4a09b746d2d40ddc
Mischa <michaeldubovoy@web.de>
2020-12-16 03:10:50 +0100

    Significantly improved speed of day 15. It's not as pretty anymore, but whatever ^^

--- day 15/day15_part2.py
+++ day 15/day15_part2.py
@@ -1,8 +1,58 @@
+from functools import reduce
 from typing import Tuple, Optional, List, Mapping
+
+from timeit import default_timer
+
+"""
+Runtime current implementation up until 10 Mio:
+17.1414813s
+
+Replacing try-except by if-else (unpythonic!, undone):
++-0.5s
+
+Baseline without printing (kept):
+-1.5s
+
+Baseline, map and tuple instead of for-loop (undone):
+-0.1s
+
+Baseline, map and reduce instead of for-loop (undone):
++0.4s
+
+Baseline, remember last spoken number in variable rather than using turn history (kept):
+-2.8s
+
+Baseline, remove history from turn history object (kept):
+-2.7s
+
+Baseline, remove last spoken number from turn history object:
+-3.1s
+
+Baseline, store only one turn per spoken number (kept!!):
+-16.4!!!
+
+Baseline, remove get older times spoken method (kept) (what happened?? random fluctuations?):
+-9.4
+
+Baseline, don't use class (kept):
+-13.4
+
+Baseline, retry if-else instead of try-except - same result (undone):
+-13.5
+
+*Correct switching places of starting_nums and enumeration*
+ 
+Baseline, use strings as dict keys (undone):
+-11.0
+
+
+"""
+
+_TEMP_BASELINE = 17.1414813
 
 
 RUN_TEST = False
-TEST_SOLUTIONS = [175594, 2578, 3544142, 261214, 6895259, 18, 362]
+TEST_SOLUTIONS = [175594, 2578, 3544142, 261214, 6895259, 18, 362]  # 436 (for 2020)
 TEST_INPUT_FILE = 'test_input_day_15.txt'
 INPUT_FILE = 'input_day_15.txt'
 
@@ -11,88 +61,42 @@ TURNS_LIM = 30000000
 ARGS = [TURNS_LIM]
 
 
-class TurnHistory:
-    def __init__(self, starting_nums: List[int]):
-        self.history = starting_nums.copy()
-        # store last two times the number was spoken as tuple: (older, newer)
-        self.last_times_spoken_dict: Mapping[int, Tuple[Optional[int], int]] = dict()
-        for ind, num in enumerate(starting_nums, start=1):
-            self.last_times_spoken_dict[num] = (None, ind)
-        self.last_spoken_num = starting_nums[-1]
-
-    def record(self, num: int, turn: int) -> None:
-        try:
-            recent_entry = self.last_times_spoken_dict[num][1]
-        except KeyError:
-            recent_entry = None
-        self.last_times_spoken_dict[num] = (recent_entry, turn)
-        self.last_spoken_num = num
-
-    def get_recent_time_spoken(self, num) -> int:
-        return self.last_times_spoken_dict[num][1]
-
-    def get_older_time_spoken(self, num) -> Optional[int]:
-        return self.last_times_spoken_dict[num][0]
-
-    def get_last_spoken_num(self):
-        return self.last_spoken_num
-
-
-def main_part1(input_file, turns_lim):
+def main_part2(input_file, turns_lim):
     with open(input_file) as file:
         lines = list(map(lambda line: line.rstrip(), file.readlines()))
-
-    # store members of form {number : (turn when it was last spoken, number of times it was spoken)}
-    # mem_dict = dict((num, (turn, 1)) for num, turn in zip(starting_nums, range(1, len(starting_nums) + 1)))
-    # last_spoken_num = starting_nums[-1]
-    # for turn in range(len(starting_nums) + 1, TURNS_LIM + 1):
-    #     if mem_dict[last_spoken_num][1] == 1:
-    #         spoken_num = 0
-    #     else:
-    #
 
     if RUN_TEST:
         solutions = []
         for line in lines:
-            print(line)
-
-            starting_nums = get_starting_nums(line)
-            turn_history = TurnHistory(starting_nums)
-            next_turn = len(starting_nums) + 1
-            for turn in range(next_turn, turns_lim + 1):
-                if turn % 1000000 == 0:
-                    print(turn)
-
-                last_spoken_num = turn_history.get_last_spoken_num()
-                if turn_history.get_older_time_spoken(last_spoken_num) is None:
-                    spoken_num = 0
-                else:
-                    prev_turn = turn_history.get_recent_time_spoken(last_spoken_num)
-                    spoken_num = prev_turn - turn_history.get_older_time_spoken(last_spoken_num)
-                turn_history.record(spoken_num, turn)
-
-            solutions.append(turn_history.get_last_spoken_num())
+            solution = play_game(line, turns_lim)
+            solutions.append(solution)
         return solutions
+    else:
+        solution = play_game(lines[0], turns_lim)
+        return solution
 
 
-    starting_nums = get_starting_nums(lines[0])
-    turn_history = TurnHistory(starting_nums)
-    next_turn = len(starting_nums) + 1
-    for turn in range(next_turn, turns_lim + 1):
-        if turn % 1000000 == 0:
-            print(turn)
-
-        last_spoken_num = turn_history.get_last_spoken_num()
-        if turn_history.get_older_time_spoken(last_spoken_num) is None:
+def play_game(line, turns_lim):
+    starting_nums = get_starting_nums(line)
+    # store last times the number was spoken
+    last_times_spoken_dict = dict(zip(starting_nums,
+                                      range(1, len(starting_nums) + 1)))
+    last_time_spoken = None  # assume that starting nums contain no duplicates
+    for turn in range(len(starting_nums) + 1, turns_lim + 1):
+        if last_time_spoken is None:
             spoken_num = 0
         else:
-            prev_turn = turn_history.get_recent_time_spoken(last_spoken_num)
-            spoken_num = prev_turn - turn_history.get_older_time_spoken(last_spoken_num)
-        turn_history.record(spoken_num, turn)
+            spoken_num = (turn - 1) - last_time_spoken
 
-    solution = turn_history.get_last_spoken_num()
+        try:
+            last_time_spoken = last_times_spoken_dict[spoken_num]
+        except KeyError:
+            last_time_spoken = None
+
+        last_times_spoken_dict[spoken_num] = turn
+
+    solution = spoken_num
     return solution
-
 
 
 def get_starting_nums(line):
@@ -101,9 +105,12 @@ def get_starting_nums(line):
 
 if __name__ == '__main__':
     if RUN_TEST:
-        solution = main_part1(TEST_INPUT_FILE, *ARGS)
+        solution = main_part2(TEST_INPUT_FILE, *ARGS)
         print(solution)
         assert (TEST_SOLUTIONS == solution)
     else:
-        solution = main_part1(INPUT_FILE, *ARGS)
-        print(solution)
+        t1 = default_timer()
+        solution = main_part2(INPUT_FILE, *ARGS)
+        t2 = default_timer()
+        t_diff = t2 - t1
+        print(solution, '\n', t_diff)  #  - _TEMP_BASELINE)
